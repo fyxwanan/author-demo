675eb30b0bb51652a2c331739a5ceaadc4773cb9
20524 <kaicheng.liu@hand-china.com>
2020-05-30 01:27:45 +0800

    时间

---
+++ build/precache-manifest.a31f6a668996460de74cd9a381aad382.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa1783fcb62ac2dd949849b79b2c2ea",
+    "url": "/author-demo/index.html"
+  },
+  {
+    "revision": "931bcfa805567bb0656d",
+    "url": "/author-demo/static/css/main.f2fdb768.chunk.css"
+  },
+  {
+    "revision": "bc39b5015bd0af48f1b0",
+    "url": "/author-demo/static/js/2.5e2c70eb.chunk.js"
+  },
+  {
+    "revision": "931bcfa805567bb0656d",
+    "url": "/author-demo/static/js/main.2b5f8d3c.chunk.js"
+  },
+  {
+    "revision": "4b2f12e1f7cfa915e635",
+    "url": "/author-demo/static/js/runtime~main.347c4b63.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -7,6 +7,93 @@ const ObjectList =
     {id: 3, name: '香蕉'},
     {id: 4, name: '菠萝'}];
 
+
+    // 时间戳
+function dateToTimestamp (time, language = 'zh_CN') {
+	console.log('a ===>')
+  // 刚刚、几分钟前、几小时前、几天前、几周前、（超一周后且当年）月日-时分、年月日-时分
+  const now = new Date().getTime();
+  const { userAgent } = navigator;
+  // const last = new Date(knowledgeInfo.lastUpdateDate).getTime();
+  let last = '';
+  if ('msSaveOrOpenBlob' in navigator) { // 判断是ie的浏览器，调用ie文件下载的方法
+    if (time) {
+      last = new Date(Date.parse(time.replace(/-/g, '/')));
+    }
+  } else if (userAgent.indexOf('Safari') > -1) {
+    if (time) {
+      last = new Date(Date.parse(time.replace(/-/g, '/')));
+    }
+  } else {
+    last = new Date(time).getTime();
+  }
+  // const subtract = (now - last) / (1000 * 60 * 60);  // 这是时间差毫小时
+  const subtract = (now - last); // 这是时间差毫小时
+  document.write('userAgent ===>' + userAgent);
+  document.write('<br>');
+  document.write('time ===>' + time);
+  document.write('<br>');
+  document.write('last ===>' + last);
+  document.write('<br>');
+  document.write('now ===>' + now);
+  document.write('<br>');
+  document.write('subtract ===>' + subtract);
+  document.write('<br>');
+
+  const minute = 1000 * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const halfamonth = day * 15;
+  const month = day * 30;
+
+  const yearC = subtract / (month * 12);
+  const monthC = subtract / month;
+  const weekC = subtract / (7 * day);
+  const dayC = subtract / day;
+  const hourC = subtract / hour;
+  const minC = subtract / minute;
+
+  const t = time.split(' ');
+  const yy = t[0].split('-')[0];
+  const mm = t[0].split('-')[1];
+  const dd = t[0].split('-')[2];
+  const hh = t[1].split(':')[0];
+  const mmm = t[1].split(':')[1];
+  if (language === 'zh_CN') {
+    if (yearC >= 1) {
+      // return KnowledgeSearchStore.language['create.year.before'].replace('${}', parseInt(yearC, 10));
+      return `${yy}-${mm}-${dd} ${hh}:${mmm}`; // 年月日-时分
+    } if (monthC >= 1) {
+      return `${mm}-${dd} ${hh}:${mmm}`; // 当年的 月日-时分
+    } if (weekC >= 1) {
+      return `${parseInt(weekC, 10)}周前`; // 几周
+    } if (dayC >= 1) {
+      return `${parseInt(dayC, 10)}天前`; // 几天
+    } if (hourC >= 1) {
+      return `${parseInt(hourC, 10)}小时前`; // 小时
+    } if (minC >= 1) {
+      return `${parseInt(minC, 10)}分钟前`; // 分钟
+    }
+    return '刚刚';
+  } else {
+    if (yearC >= 1) {
+      // return KnowledgeSearchStore.language['create.year.before'].replace('${}', parseInt(yearC, 10));
+      return `${yy}-${mm}-${dd} ${hh}:${mmm}`; // 年月日-时分
+    } if (monthC >= 1) {
+      return `${mm}-${dd} ${hh}:${mmm}`; // 当年的 月日-时分
+    } if (weekC >= 1) {
+      return `${parseInt(weekC, 10)} week ago`; // 几周
+    } if (dayC >= 1) {
+      return `${parseInt(dayC, 10)} days ago`; // 几天
+    } if (hourC >= 1) {
+      return `${parseInt(hourC, 10)} hours ago`; // 小时
+    } if (minC >= 1) {
+      return `${parseInt(minC, 10)} minutes ago`; // 分钟
+    }
+    return 'just';
+  }
+};
+
 class App extends Component{
 
     constructor(props) {
@@ -19,6 +106,9 @@ class App extends Component{
 
     componentDidMount() {
         this.getBoxIds();
+        const b = dateToTimestamp('2020-05-22 18:16:51');
+        console.log('b ===>', b)
+        document.write(b);
     }
 
     /**
